d865e7176a862db09e2a60349832f61b8fe45956
ckrrajeshkumar <ckrrajeshkumar@gmail.com>
2025-07-01 16:47:29 -0400

    aaa

--- remote-state-file/main.tf
+++ remote-state-file/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "babaji-web"{
   ami = var.amiid
   instance_type = var.intype
-  security_groups = var.sgid
+  vpc_security_group_ids = var.sgid
   tags={
     Name = var.ec2name
   }
